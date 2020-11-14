cca10c1379b895d5d3e318b96637efa0a4e5e5c8
[afzal tj] <[afzaltj81@gmail.com]>
2020-11-13 23:21:17 -0500

    website about cars
    fast cars vromm

--- Main.js
+++ Main.js
@@ -1,3 +1,15 @@
-var car = [Ferrari]
-var car = [LexusLFA]
-var car = [Porshe918Spider]
+let lexushover = document.getElementById("LexusLFA");
+lexushover.addEventListener("mouseenter", function( event ) {   
+  event.target.style.filter = "grayscale(100%)"; 
+  setTimeout(function() {
+    event.target.style.color = "white";
+  }, 500);s
+}, false);
+
+let Ferrarihover = document.getElementById("Ferari");
+lexushover.addEventListener("mouseenter", function( event ) {   
+  event.target.style.filter = "grayscale(100%)"; 
+  setTimeout(function() {
+    event.target.style.color = "white";
+  }, 500);s
+}, false);
