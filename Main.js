ecf185fe725237402015d3267c355c640fd9eb20
shydroid1 <73849464+shydroid1@users.noreply.github.com>
2020-11-23 10:06:49 -0500

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,23 +1,15 @@
 let lexushover = document.getElementById("LexusLFA");
-lexushover.addEventListener("mouseenter", function( event ) {
+lexushover.addEventListener("mouseenter", function( event ) {   
   event.target.style.filter = "grayscale(100%)"; 
-  setTimeout(function(_r) {
+  setTimeout(function() {
     event.target.style.color = "white";
   }, 500);s
 }, false);
 
-let Sianroadsterhover = document.getElementById("Sian-Roadster");
-Sianroadsterhover.addEventListener("mouseenter", function( event ) {   
+let Ferrarihover = document.getElementById("Ferari");
+lexushover.addEventListener("mouseenter", function( event ) {   
   event.target.style.filter = "grayscale(100%)"; 
-  setTimeout(function(_r) {
-    event.target.style.color = "white";
-  }, 500);s
-}, false);
-
-let Porschehover = document.getElementById("Porsche 918 Spider");
-Porschehover.addEventListener("mouseenter", function( event ) {   
-  event.target.style.filter = "grayscale(100%)"; 
-  setTimeout(function(_r) {
+  setTimeout(function() {
     event.target.style.color = "white";
   }, 500);s
 }, false);
