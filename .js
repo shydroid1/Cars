df62b729481caed16dfa88778a158c21d93464c3
Snagger <[afzaltj81@gmail.com]>
2020-11-29 10:50:19 -0500

    cars

--- .js
+++ .js
@@ -1,17 +1,4 @@
-var cars = ["Ferrari", "Lexus LFA", "Porsche Spider", "Sian-Roadster"];
-let lexushover = document.getElementById("LexusLFA");
-lexushover.addEventListener("mouseenter", function( event ) {   
-  event.target.style.filter = "grayscale(100%)"; 
-  setTimeout(function() {
-    event.target.style.color = "white";
-  }, 500);s
-}, false);
-
-let Ferrarihover = document.getElementById("Ferari");
-lexushover.addEventListener("mouseenter", function( event ) {   
-  event.target.style.filter = "grayscale(100%)"; 
-  setTimeout(function() {
-    event.target.style.color = "white";
-  }, 500);s
-}, false);
- 
+document.getElementById("Car").innerHTML = Car
+Car = ["Ferrari","LexusLFA","Porsche 918 Spider","Sian-Roadster",""]
+var Car
+console.log(Car)
