1db5250d4f15853e07453e2ca8aa5407803c9035
Snagger <[afzaltj81@gmail.com]>
2020-11-30 08:42:17 -0500

    Cars
    added var array

--- Main.js
+++ Main.js
@@ -1,4 +1,10 @@
-document.getElementById("Car").innerHTML = Car
+document.getElementById("Car").innerHTML = Car;
 Car = ["Ferrari","LexusLFA","Porsche 918 Spider","Sian-Roadster",""]
+var text = "";
+var i;
+for (i = 0; i <Car.length; i++) {
+    text +=car[i] + "<br>";
+    document.getElementById("Car").innerHTML = text;
+}
 var Car
 console.log(Car)
